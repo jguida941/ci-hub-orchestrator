9240358f57f1f89b74097a44ca7212687cd64d16
Justin <justin.guida@snhu.edu>
2025-10-23 18:46:41 -0400

    Use direct release binaries for oras, cosign, syft

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ORAS_VERSION=1.2.0
+COSIGN_VERSION=v2.2.4
+SYFT_VERSION=v1.18.0
+TMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TMP_DIR"' EXIT
+
 # Install oras
-curl -sSL https://oras.land/install.sh | bash
-export PATH="$HOME/.oras/bin:$PATH"
+curl -fsSLo "$TMP_DIR/oras.tar.gz" "https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_linux_amd64.tar.gz"
+tar -xzf "$TMP_DIR/oras.tar.gz" -C "$TMP_DIR" oras
+chmod +x "$TMP_DIR/oras"
+sudo mv "$TMP_DIR/oras" /usr/local/bin/oras
 
 # Install cosign
-curl -sSfL https://raw.githubusercontent.com/sigstore/cosign/main/install.sh \
-  | COSIGN_INSTALL_DIR=$HOME/.cosign sh
-export PATH="$HOME/.cosign:$PATH"
+curl -fsSLo "$TMP_DIR/cosign" "https://github.com/sigstore/cosign/releases/download/${COSIGN_VERSION}/cosign-linux-amd64"
+chmod +x "$TMP_DIR/cosign"
+sudo mv "$TMP_DIR/cosign" /usr/local/bin/cosign
 
 # Install syft
-curl -sSfL https://github.com/anchore/syft/releases/download/v1.18.0/syft_1.18.0_linux_amd64.tar.gz \
-  | tar -xz -C /usr/local/bin syft
+curl -fsSLo "$TMP_DIR/syft.tar.gz" "https://github.com/anchore/syft/releases/download/${SYFT_VERSION}/syft_${SYFT_VERSION}_linux_amd64.tar.gz"
+tar -xzf "$TMP_DIR/syft.tar.gz" -C "$TMP_DIR" syft
+chmod +x "$TMP_DIR/syft"
+sudo mv "$TMP_DIR/syft" /usr/local/bin/syft
